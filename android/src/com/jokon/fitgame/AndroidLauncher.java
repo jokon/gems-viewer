518e21552540edc15e84c649621d28a70a4a1d63
Jaroslaw Okonski <jaroslaw.okonski@gmail.com>
2024-03-03 22:36:32 +0100

    Init libgtx content base

---
+++ android/src/com/jokon/fitgame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jokon.fitgame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jokon.fitgame.FitGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FitGame(), config);
+	}
+}
---
+++ desktop/src/com/jokon/fitgame/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jokon.fitgame;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.jokon.fitgame.FitGame;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("FitGame");
+		new Lwjgl3Application(new FitGame(), config);
+	}
+}
