ea9c0d0b64c71da6e7bb7a2779fc964d1f33e54a
Jaroslaw Okonski <jaroslaw.okonski@gmail.com>
2024-03-09 23:08:28 +0100

    Init work on blutooth connection

--- android/src/com/jarecki/AndroidLauncher.java
+++ android/src/com/jarecki/AndroidLauncher.java
@@ -1,5 +1,10 @@
 package com.jarecki;
 
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -7,10 +12,37 @@ import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.jarecki.TreadmillChallenge;
 
 public class AndroidLauncher extends AndroidApplication {
+	private static final int REQUEST_ENABLE_BT = 0;
+
 	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new TreadmillChallenge(), config);
+
+
+		BluetoothManager bluetoothManager = null;
+		if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+			bluetoothManager = getSystemService(BluetoothManager.class);
+			BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
+			if (bluetoothAdapter == null) {
+				// Device doesn't support Bluetooth
+			}
+			if (!bluetoothAdapter.isEnabled()) {
+				Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+				if (checkSelfPermission(Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+					// TODO: Consider calling
+					//    Activity#requestPermissions
+					// here to request the missing permissions, and then overriding
+					//   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+					//                                          int[] grantResults)
+					// to handle the case where the user grants the permission. See the documentation
+					// for Activity#requestPermissions for more details.
+					return;
+				}
+				startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+			}
+		}
+
 	}
 }
